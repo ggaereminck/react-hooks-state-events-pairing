ac8d7480db750793bf1049a5037d178623cff28f
Gerald <ggaereminck@yahoo.com>
2021-09-23 13:01:43 -0400

    added button for showing and hiding comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-
+import { useState } from "react";
 import VideoContainer from "./VideoContainer"
 import CommentContainer from "./CommentContainer"
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true)
+  
   return (
     <div className="App">
       <VideoContainer/>
-      <CommentContainer/>
+      <button onClick={() => setIsVisible(!isVisible)}>{isVisible ? 'Hide Comments' : 'Show Comments'}</button>
+      {isVisible ? <CommentContainer/> : null}
     </div>
   );
 }
